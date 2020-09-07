25a8bc0c53e8d47c2ae86c9b62826860532d27f4
Locrz <locrz@MBP-de-Danilo-Pela.local>
2020-09-07 13:51:12 -0300

    Create transactions services

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const { total } = await transactionsRepository.getBalance();
+    const isOutcome = type === 'outcome';
+
+    if (isOutcome && value > Number(total)) {
+      throw new AppError('You dont have enough balance for this transaction');
+    }
+
+    const categoriesRepository = getRepository(Category);
+
+    let categoryFinded = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!categoryFinded) {
+      const newCategory = categoriesRepository.create({ title: category });
+      categoryFinded = await categoriesRepository.save(newCategory);
+    }
+
+    const category_id = categoryFinded.id;
+    const transaction = transactionsRepository.create({
+      title,
+      category_id,
+      type,
+      value,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,28 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import { isUuid } from 'uuidv4';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    if (!isUuid(id)) {
+      throw new AppError('Transaction id is invalid.');
+    }
+
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('No transaction was founded with the given id.');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
